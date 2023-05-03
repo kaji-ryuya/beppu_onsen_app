054e8e6b5b5e03951fc9ba83807de08811db47f4
ryuya kaji <kaji.777r@gmail.com>
2023-05-03 16:52:36 +0900

    add: Spasコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'spas/index'
+  get 'spas/show'
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
